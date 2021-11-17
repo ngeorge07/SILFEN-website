85da50da8539bb962a0324683382c73a1cdaca96
George <geor2335@stud.kea.dk>
2021-11-17 11:46:03 +0100

    fixed nav on firefox

--- scripts/product.js
+++ scripts/product.js
@@ -8,3 +8,24 @@ const swiper = new Swiper(".product-swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function infoOpen() {
+  const description = document.querySelector("#description");
+  const details = document.querySelector("#details");
+  const descArrow = description.querySelector("svg");
+  const detArrow = document.querySelector("svg");
+
+  description.addEventListener("click", () => {
+    document.querySelector("#description-p").classList.toggle("info-active");
+    if (descArrow.classList.contains != "rotate") {
+      console.log("it works");
+      descArrow.classList.add("rotate");
+    }
+  });
+
+  details.addEventListener("click", () => {
+    document.querySelector("#details-p").classList.toggle("info-active");
+  });
+}
+
+infoOpen();
